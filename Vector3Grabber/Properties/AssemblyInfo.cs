a71ee6d93c668e765ce64dd8c51122d545afc5ba
Rohit685 <mittalr685@gmail.com>
2023-02-25 12:06:10 -0600

    Updated AssemblyInfo.cs

--- Vector3Grabber/Properties/AssemblyInfo.cs
+++ Vector3Grabber/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Vector3Grabber")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("VectorGrabber")]
+[assembly: AssemblyDescription("Helps developers find locations for callouts/ambient events")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Vector3Grabber")]
+[assembly: AssemblyProduct("VectorGrabber")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
